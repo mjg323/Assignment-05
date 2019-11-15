bb35ed1205e3e0ab71731464a9955ce7b42030d2
Michael Grise <mjg323@lehigh.edu>
2019-11-14 21:26:03 -0500

    Added Homework

---
+++ GriseAssignment05.R
@@ -0,0 +1,10 @@
+############################################################
+# R script to accompany Assignment 05 
+# Written by Michael Grise                                #
+############################################################
+
+DF <- data.table::fread("FEDFUNDS.csv")
+data.table::fread("FEDFUNDS.csv")
+
+
+
